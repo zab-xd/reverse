47d3ef97c304482f9d09088855f7c65881a054b0
itsuki10 <84274139+itsuki10@users.noreply.github.com>
2021-06-09 11:19:43 +0700

    Create Enc-Lambada_out.py

---
+++ Enc-Lambada/Enc-Lambada_out.py
@@ -0,0 +1,180 @@
+# coding:utf-8
+# decompile by itsuki
+
+import os, sys, zlib, base64, marshal, binascii, time, py_compile
+import random, base64, marshal, hashlib, argparse
+from time import sleep as waktu
+from random import randint
+from os import system, name
+os.system('clear')
+version = 'KANG ARYA '
+expired = ['05', '07', '2021']
+buffer_size = 898989
+pesing = '\x1b[0;32m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ \n ▒▒▒▒▒▒▒▒▄▄▄▄▄▄▄▄▒▒▒▒▒▒ \n ▒▒█▒▒▒▄██████████▄▒▒▒▒ \n ▒█▐▒▒▒████████████▒▒▒▒ \n ▒▌▐▒▒██▄▀██████▀▄██▒▒▒ \n ▐┼▐▒▒██▄▄▄▄██▄▄▄▄██▒▒▒ \n ▐┼▐▒▒██████████████▒▒▒ \n ▐▄▐████─▀▐▐▀█─█─▌▐██▄▒ \n ▒▒█████──────────▐███▌ \n ▒▒█▀▀██▄█─▄───▐─▄███▀▒ \n ▒▒█▒▒███████▄██████▒▒▒ \n ▒▒▒▒▒██████████████▒▒▒ \n ▒▒▒▒▒██████████████▒▒▒ \n ▒▒▒▒▒█████████▐▌██▌▒▒▒ \n ▒▒▒▒▒▐▀▐▒▌▀█▀▒▐▒█▒▒▒▒▒ \n ▒▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▌▒▒▒▒▒ \n ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ \n\n     \x1b[0;31m╔════•ೋೋ•════╗  \n     \x1b[0;37m☆ۣۜۜ͜͡₲ℜ❍Ⴎℙ B!it@r♤ \n     \x1b[0;31m╚════•ೋೋ•════╝ \n'
+
+        
+def silet():
+    silet = [
+   '\x1b[1;32m██\033[0;37m▒▒▒▒▒▒▒▒▒▒10%',   '\x1b[1;34m███\033[0;37m▒▒▒▒▒▒▒▒▒20%',   '\x1b[1;36m████\033[0;37m▒▒▒▒▒▒▒▒30%',   '\x1b[1;31m█████\033[0;37m▒▒▒▒▒▒▒40%',   '\x1b[1;33m██████\033[0;37m▒▒▒▒▒▒50%',   '\x1b[1;35m███████\033[0;37m▒▒▒▒▒60%',   '\x1b[1;36m████████▒▒▒▒70%',   '\x1b[1;32m█████████\033[0;37m▒▒▒80%',   '\x1b[1;34m██████████\033[0;37m▒▒90%',   '\x1b[1;37m███████████100%\033[1;36m\n\n']
+    for o in silet:
+        print '\r\t' + o,
+        sys.stdout.flush()
+        time.sleep(0.1)
+        os.system('clear')
+        
+silet()
+        
+def login():
+    os.system('clear')
+    from datetime import datetime
+    saat_ini = datetime.now()
+    tgl = saat_ini.strftime('%d')
+    bln = saat_ini.strftime('%m')
+    thn = saat_ini.strftime('%Y')
+    tanggal = thn + bln + tgl
+    exp = expired[2] + expired[1] + expired[0]
+    if tanggal >= exp:
+        print (('\n').join(['' + pesing + '']))
+        print (('\n').join(['\x1b[1;31m ➤ \x1b[1;33mSCRIPT KADALUWARSA MBAH.. 😊\n\x1b[1;31m ➤ \x1b[1;32mAYUK HUBUNGI AUTORNYA \n \x1b[1;31m➤ \x1b[1;35mJANGAN LUPA DONASI SEIKHLASNYA 🐧 \n \x1b[1;31m➤ \x1b[1;36mSILAHKAN CEK YT ARIS SETYA CHANNEL \n \x1b[1;31m➤ \x1b[1;37mDI SUBSCRIBE YA SAYANG (☉‿☉✿)']))
+        sys.exit()
+    else:
+    	kontol()
+    
+    
+def kontol():
+    os.system('clear')
+    from datetime import datetime
+    saat_ini = datetime.now()
+    tgl = saat_ini.strftime('%d')
+    bln = saat_ini.strftime('%m')
+    thn = saat_ini.strftime('%Y')
+    tanggal = thn + bln + tgl
+    exp = expired[2] + expired[1] + expired[0]
+    if float(tanggal) + 1 == float(exp):
+        zasu.print_usage()
+    else:
+        zasu.print_usage()
+
+
+RED_MAGIC = '\x03\xf3\r\nd\x83\x8e^'
+
+class obf:
+
+	num = []
+
+	def softex_charm(self, SatuBungkusMemek = None, kontolKadal = 0, kontolMonyet = 1,misq = ""):
+		psf = [ "" , "" , "" ]
+		psf[0] += str(kontolKadal)
+		psf[2] = str(kontolMonyet)
+		psf[1] = "&"+SatuBungkusMemek+"&"
+		return misq.join(psf)
+
+	def inum_depth(self,bigF, nim, pct = [], o = 0):
+		if not "list" in type(bigF).__name__ and not "list" in type(nim).__name__:
+			raise TypeError("--- type not list --")
+		index = random.randint(2,len(bigF)/2+2)
+		if index < 5 <= len(bigF):
+			index += 2
+		foad = {"index1":bigF[:index],"index2":bigF[index:]}
+		while o < len(foad["index1"]):
+			pct.append(foad["index1"][o])
+			o += 1
+		o -= o
+		while o < len(nim):
+			pct.append(nim[o])
+			o += 1
+		o -= o
+		while o < len(foad["index2"]):
+			pct.append(foad["index2"][o])
+			o += 1
+		max_legth = len(pct[:len(foad["index1"])+len(nim)])-1
+		return {"a":len(foad["index1"]),"z":max_legth+1,"depth":pct}
+
+	def slice(self,strings=None,pt = []):
+		diff = len(strings) / 2
+		fp = {"stiff":strings[:diff],"numc":strings[diff:]}
+		for of in fp["numc"]:
+			pt.append(ord(of))
+		fop = self.selector_num(fp["stiff"])
+		co = self.inum_depth(fop,pt)
+		fo = self.swift(fp["stiff"], co["a"], co["z"])
+		return 'import base64,marshal\nDIFENT_STACK = []\n(lambda ___ : sot.sort())(globals().update({(lambda : uc[i][p] ^ c[0][o] ** 5 != "").__code__.co_consts[3].join([chr(i) for i in (lambda _______, _, ___ : [(((((___>=_______)+(___>=___)+(_>=_))+((___>_)+(_______<=_)+(_<=___))+((___>=_)-(_______>___)))<<(((___>_)+(_<=___))+((_>=_______)+(___>=___))))+(((___>=_______)+(_______!=_)+(___<=___)))),(((((___>=___)+(___>=___)+(_______!=_))+((_______!=___)+(_______==_______)+(___!=_______))+((_>=_______)*(___>=_______)))<<(((_______!=_)+(___!=_))+((___!=_)+(_______<=_______))))-(((___>=___)*(_!=___)))),(((((___>_______)+(_>=_______)+(___>=___))+((_<___)+(_______<_)+(___!=_))+((_______>=_______)*(___!=_)))<<(((_______<___)+(_______<___))+((_______!=___)+(___!=_______))))+((((_==_)-(_______==_)))<<(((___!=_)+(_<___)))))])((((0x83273a==0x83273a)+(0b1001!=0o1663133)+(0x83273a!=0b1001))),((((((((((((0b1001>=0x83273a)+(0o1663133<0x83273a)))<<(((0x83273a<=0x83273a)+(0b1001>=0b1001)+(0o1663133!=0x83273a))))+(((0o1663133!=0x83273a)+(0b1001>0b1001)))))))))<<(((0b1001>0b1001)+(0b1001<=0b1001))))),(((((0x83273a==0x83273a)+(0b1001==0b1001)+(0b1001<=0x83273a))+((0x83273a>=0x83273a)+(0b1001<0x83273a)+(0o1663133!=0b1001))+((0b1001<=0o1663133)-(0x83273a<0b1001)))<<(((0b1001<=0o1663133)+(0x83273a<=0x83273a))))-(((0b1001!=0o1663133)*(0o1663133<=0x83273a)))))]):(lambda : [{int(c.split("$")[0])+int(c.split("$")[2]):c.split("$")[1]} for c in (lambda ___ : ___.split("@"))(\''+str(fo)+'\')])()}))\n(lambda : globals().update({(lambda : uc[i][p] ^ c[0][o] ** 5 != "").__code__.co_consts[3].join([chr(i) for i in (lambda ______, __, ____ : [((((((((____==____)+(______!=__)+(____>=__))+((____!=__)+(____>__)+(__<____))+((______>=__)*(______<=____)))<<(((__<=______)+(__==__))))-(((____>=__)*(__<____)))))<<(((__<____)+(____>______))))-(((______<=______)-(__>____)))),((((((((____>=__)+(______!=____)+(__>=__)))<<(((______!=__)+(______!=____)+(____>=______))))+(((__!=______)-(______>____)))))<<(((______<=______)+(____>__))))+(((______>=____)+(____!=__)))),((((((((____<=____)+(__!=__)))<<(((____>=______)+(______!=__))+((____>=____)+(__!=____))))-(((______!=____)+(__==____)))))<<(((__>=__)+(____<=____)+(____!=______))))+(((__==__)+(____<=__))))])((((((0o1663133!=0b1001)*(0b1001>=0b1001)))<<(((0x83273a>=0b1001)+(0x83273a!=0o1663133))+((0x83273a!=0b1001)+(0b1001!=0x83273a))))),(((((0x83273a>0o1663133)+(0o1663133<=0x83273a)+(0x83273a>0b1001))+((0x83273a==0x83273a)+(0x83273a>0b1001)+(0b1001!=0x83273a))+((0o1663133==0o1663133)+(0b1001>=0o1663133)))<<(((0b1001<0o1663133)*(0x83273a==0x83273a))))),(((((0b1001<=0o1663133)+(0o1663133<=0x83273a))+((0o1663133>0b1001)+(0x83273a<=0x83273a))+((0b1001<=0o1663133)+(0x83273a==0o1663133)))<<(((0x83273a<=0x83273a)+(0b1001!=0o1663133))))))]):(lambda cz : cz[::-1].split("&"))((lambda x : (lambda : uc[i][p] ^ c[0][o] ** 5 != "").__code__.co_consts[3].join([c[1][c[0]] for c in enumerate(x)]))(sot))}))()\nMAX_LENGTH = int(key[0])\n___ = '+ str(co["depth"]) +'\nwhile MAX_LENGTH<int(key[2]):\n    DIFENT_STACK.append(___[MAX_LENGTH])\n    MAX_LENGTH += 1\nexec(marshal.loads((lambda ____ : base64.b64decode(____[1]))(key)+(lambda : uc[i][p] ^ c[0][o] ** 5 != "").__code__.co_consts[3].join([chr(i) for i in DIFENT_STACK])))\ndel DIFENT_STACK,MAX_LENGTH,___,key,sot'
+
+	def selector_num(self,code,tig=1):
+		# by arya :v
+		coprez = len(code) * 8
+		if coprez > 100:
+			coprez += len(code) * 10
+		else:
+			if coprez > 200:
+				coprez += len(code) * 9
+			else:
+				if coprez > 300:
+					coprez += len(code) * 8
+				else:
+					if coprez > 400:
+						coprez += len(code) * 7
+					else:
+						if coprez > 500:
+							coprez += len(code) * 6
+						else:
+							pass
+		for cin in range(coprez/2):
+			civ = random.randint(tig,215)
+			if civ > tig*0x100-cin:
+				civ += 5
+				if civ >= len(code) * 1 ^ tig:
+					civ += coprez % 0x2fd6f ^ 5 / 52
+			else:
+				civ += coprez << 2 / len(code) ^ 2
+			left = civ != coprez
+			if left is True:
+				civ * 5
+			self.num.append(civ/2/2)
+		return self.num
+
+	def obfuscate(self,file=None,out = ["",False]):
+		fov = marshal.dumps(compile(open(file).read(),"ARIS-STYA-CHANNEL","exec"))
+		MAC = self.slice(fov)
+		if out[1]:
+			f = open(out[0],"w")
+		else:
+			f = open(file.replace(".py","_enc.py"),"w")
+		id = str(hash(MAC))
+		key = hashlib.md5(str(hash(id))).hexdigest()
+		f.write(RED_MAGIC+marshal.dumps(compile(MAC,"ARIS-SETYA-CHANNEL","exec")))
+		f.close()
+
+	def swift(self,code=None, min = None ,max = None):
+		n = 3 if code is None else 1
+		if (lambda : stif[5] ^ {x:diff-5} * pf[0][i][f]["diff"][""]).__code__.co_consts[4] == code:
+			return ""
+		kod = base64.b64encode(code)
+		kod = self.softex_charm(kod,min,max)
+		kod = kod[::-1]
+		kodsp = [kod[i:i+n] for i in range(0, len(kod), n)]
+		kod = []
+		for nn,i in enumerate(kodsp):
+			if nn!=0:
+				nb=random.randint(-nn,nn)
+				kod.append(str(nb)+"$"+i+"$"+str(nn-nb))
+			else:
+				kod.append("0$"+i+"$0")
+		kod=random.sample(kod,len(kod))
+		kod = "@".join(kod)
+		return kod
+
+zasu = argparse.ArgumentParser("\n\n\x1b[1;32m █▀▀ █▄─█ ▄▀ █▀▀▄ ▀▄─▄▀ █▀▄ ▀█▀\n █▀▀ █─▀█ █─ █▐█▀ ──█── █─█ ─█─\n ▀▀▀ ▀──▀ ─▀ ▀─▀▀ ──▀── █▀─ ─▀─ \n\n \x1b[1;37m🔐 Compile Python2\n \x1b[1;34m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n\033[1;36mlambada.py -i scrip.py -o sc nama out",description="Obfuscate python2")
+zasu.add_argument("-i","--file",metavar="",help="specify the file name to process")
+zasu.add_argument("-o","--out",metavar="",help="outfile save obfuscate")
+zof = zasu.parse_args()
+if zof.file:
+	if zof.out:
+		obf().obfuscate(zof.file,out=[zof.out,True])
+		print ("\n\033[1;37mBerhasil Encrypt \033[0;36mNama \033[1;34m>\033[1;32m %s "%(zof.out))
+	else:
+		obf().obfuscate(zof.file)
+		print ("\nBerhasil Encrypt \033[0;36mNama \033[1;34m>\033[1;32m %s "%(zof.file.replace(".py","_enc.py")))
+else:
+	login()
+
